dfcc60b7df8b4bed63488a413b6b57fbb347b5ab
Mr. Galt <135198406+Mr-Galt@users.noreply.github.com>
2023-06-21 11:25:39 +0400

    Update ft_putnbr.c
    Updated ex07, seg fault is no more

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -10,27 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	writing(int a)
+void	ft_putchar(char c)
 {
-	write(1, &a, 1);
+	write(1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
 	if (nb < 0)
 	{
-		writing('-');
-		ft_putnbr(-nb);
+		ft_putchar('-');
+		nb *= -1;
 	}
-	else if (nb > 9)
+	if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
 	}
 	else
-	{	
-		writing(nb + '0');
+	{
+		ft_putchar(nb + '0');
 	}
 }
